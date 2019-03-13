4d11e47b7cca4fa0621642fad33d6ee784aa53ac
xgqfrms <xgqfrms@outlook.com>
2019-03-13 10:44:13 +0800

    GPS

---
+++ GPS-converter.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2018-01-01
+ *
+ * @description GPS converter
+ * @augments
+ * @example
+ *
+ */
+
+const GPSconverter = (datas = [], debug = false) => {
+    let result = ``;
+    // do something...
+    return result;
+};
+
+// 30° 16' 41.982" N
+
+let l = `30° 16' 41.982" N`;
+
+let l_arr = l.split(` `);
+
+
+
+30 + 16/60 + 41.982 / 3600
+
+// 120° 1' 8.908" E
+
+
+120 + 1/60 + 8.908 / 3600
+
+
+
+export default GPSconverter;
+
+export {
+    GPSconverter,
+};
